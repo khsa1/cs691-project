ddd5e56dbd9da617baded47c1d1cd8363a279ccc
John Clemens <john@deater.net>
2015-03-22 17:04:40 -0400

    Checkpoint checking
    Still need projection and min_subgraph

--- gspan-glib/gspan.c
+++ gspan-glib/gspan.c
@@ -621,15 +621,255 @@ int judge_backwards(struct gspan *gs, GList *right_most_path, GList *projection,
 			free_history(h);
 		}
 
-		l3 = g_hash_table_get_keys(pm_backwards);
-		if (g_list_length(l3) > 0) {
-			g_list_free(l3);
+		if (g_hash_table_size(pm_backwards) > 0)
 			return 1;
-		}
-		g_list_free(l3);
 	}
 
 	return 0;
+}
+
+int judge_forwards(struct gspan *gs, GList *right_most_path, GList *projection,
+					GHashTable *pm_forwards, int min_label)
+{
+	int i, rmp0;
+	struct history *h;
+	GList *l1, *l2, *l3, *l4, *values = NULL;
+
+	rmp0 = 	GPOINTER_TO_INT(g_list_nth_data(right_most_path, 0));
+
+	for(i=0,l1 = g_list_first(projection); l1; l1 = g_list_next(l1)) {
+		struct pre_dfs *p = (struct pre_dfs *)l1->data;
+		struct node *last_node;
+		struct edge *last_edge;
+	
+		h = alloc_history();
+		build_history(h,p);
+
+		last_edge = g_list_nth_data(h->edges, rmp0);
+		last_node = graph_get_node(gs->min_graph, last_edge->to);
+
+		for (l2 = g_list_first(last_node->edges); l2; 
+						l2 = g_list_next(l2)) {
+			struct edge *e = (struct edge *)l2->data;
+			struct node *to_node;
+			struct dfs_code *dfsc;
+			struct pre_dfs *npdfs;
+			int to_id;
+			
+			to_node = graph_get_node(gs->min_graph, e->to);
+
+			if (g_hash_table_contains(h->has_nodes, 
+						GINT_TO_POINTER(e->to)) || 
+						to_node->label < min_label)
+				continue;
+			
+			to_id = ((struct dfs_code *)g_list_nth_data(
+						gs->min_dfs_codes, rmp0))->to;
+
+			dfsc = malloc(sizeof(struct dfs_code));
+			if (!dfsc) {
+				perror("malloc dfsc in jf()");
+				exit(1);
+			}
+			dfsc->from = to_id;
+			dfsc->to = to_id + 1;
+			dfsc->from_label = last_node->label;
+			dfsc->edge_label = e->label;
+			dfsc->to_label = to_node->label;
+
+			npdfs = malloc(sizeof(struct pre_dfs));
+			if (!npdfs) {
+				perror("malloc npdfs in jf()");
+				exit(1);
+			}
+			npdfs->id = 0;
+			npdfs->edge = e;
+			npdfs->prev = p;
+
+			if (g_hash_table_contains(pm_forwards, dfsc))
+				values = g_hash_table_lookup(
+							pm_forwards,dfsc);
+			values = g_list_append(values, npdfs);
+			g_hash_table_insert(pm_forwards, dfsc, values);
+
+		}
+
+		free_history(h);
+	}
+
+	
+	if (g_hash_table_size(pm_forwards) == 0) {
+		for(l1 = g_list_first(right_most_path); l1; 
+							l1 = g_list_next(l1)) {
+			int rmp = GPOINTER_TO_INT(l1->data);
+			
+			for (l2=g_list_first(l2); l2; l2 = g_list_next(l2)) {
+				struct pre_dfs *p = (struct pre_dfs *)l2->data;
+				struct edge *cur_edge;
+				struct node *cur_node;
+				struct node *cur_to;
+
+				h = alloc_history();
+				build_history(h, p);
+
+				cur_edge = g_list_nth_data(h->edges, rmp);
+				cur_node = graph_get_node(gs->min_graph, 
+								cur_edge->from);
+				cur_to = graph_get_node(gs->min_graph, 
+								cur_edge->to);
+
+				for (l3 = g_list_first(cur_node->edges); l3;
+							l3 = g_list_next(l3)) {
+					struct edge *e = (struct edge *)l3->data;
+					struct node *to_node;
+
+					to_node = graph_get_node(gs->min_graph, e->to);
+
+					if (cur_edge->to == to_node->id || 
+							g_hash_table_contains(h->has_nodes, 
+									GINT_TO_POINTER(e->id)) || 
+									to_node->label < min_label)
+						continue;
+
+					if (cur_edge->label < e->label || 
+							(cur_edge->label == e->label &&
+							cur_to->label <= to_node->label)) {
+						int from_id, to_id;
+						struct dfs_code *dfsc;
+						struct pre_dfs *npdfs;
+
+						from_id = ((struct dfs_code *)g_list_nth_data(
+								gs->min_dfs_codes, rmp))->from;
+
+						to_id = ((struct dfs_code *)g_list_nth_data(
+								gs->min_dfs_codes, rmp0))->to;
+
+						dfsc = malloc(sizeof(struct dfs_code));
+						if (!dfsc) {
+							perror("malloc dfsc in jf()");
+							exit(1);
+						}
+						dfsc->from = from_id;
+						dfsc->to = to_id + 1;
+						dfsc->from_label = cur_node->label;
+						dfsc->edge_label = e->label;
+						dfsc->to_label = to_node->label;
+
+						npdfs = malloc(sizeof(struct pre_dfs));
+						if (!npdfs) {
+							perror("malloc npdfs in jf()");
+							exit(1);
+						}
+						npdfs->id = 0;
+						npdfs->edge = e;
+						npdfs->prev = p;
+
+						if (g_hash_table_contains(pm_forwards, dfsc))
+							values = g_hash_table_lookup(
+									pm_forwards,dfsc);
+						values = g_list_append(values, npdfs);
+						g_hash_table_insert(pm_forwards, dfsc, values);
+					}
+				}
+
+				free_history(h);
+			}
+
+			if (g_hash_table_size(pm_forwards) > 0) 
+				break;
+		}
+	}
+
+	if (g_hash_table_size(pm_forwards) > 0) 
+		return 1;
+	return 0;
+}
+
+
+
+int projection_min(struct gspan *gs, GList *projection)
+{
+	GList *right_most_path, *l1, *keys, *values = NULL;
+	int min_label;
+	int ret;
+	GHashTable *pm_backwards, *pm_forwards;
+
+	right_most_path = build_right_most_path(gs->min_dfs_codes);
+	min_label = ((struct dfs_code *)
+				g_list_first(gs->min_dfs_codes))->from_label;
+
+	pm_backwards = g_hash_table_new(dfs_code_hash, glib_dfs_code_equal);
+	pm_forwards = g_hash_table_new(dfs_code_hash, glib_dfs_code_equal);
+
+	ret = judge_backwards(gs, right_most_path, projection, pm_backwards);
+	if (ret) {
+		keys = g_hash_table_get_keys(pm_backwards);
+		keys = g_list_sort(keys, (GCompareFunc)dfs_code_backward_compare);
+		for (l1 = g_list_first(keys); l1; l1 = g_list_next(l1)) {
+			struct dfs_code *dfsc = (struct dfs_code *)l1->data;
+
+			gs->min_dfs_codes = g_list_append(gs->min_dfs_codes, 
+									dfsc);
+			if (!dfs_code_equal(
+				g_list_nth_data(gs->dfs_codes, 
+					g_list_length(gs->min_dfs_codes)-1), 
+					g_list_last(gs->min_dfs_codes)->data)) {
+
+				g_list_free(keys);
+				cleanup_map(pm_backwards);
+				g_list_free(right_most_path);
+				return 0;
+			}
+		
+			values = g_hash_table_lookup(pm_backwards, dfsc);
+			ret = projection_min(gs, values);
+
+			g_list_free(keys);
+			cleanup_map(pm_backwards);
+			cleanup_map(pm_forwards);
+			g_list_free(right_most_path);
+			return ret;
+		}
+		g_list_free(keys);
+	}
+	
+	ret = judge_forwards(gs, right_most_path, projection, 
+						pm_forwards, min_label);
+	if (ret) {
+		keys = g_hash_table_get_keys(pm_forwards);
+		keys = g_list_sort(keys, (GCompareFunc)dfs_code_forward_compare);
+		for (l1 = g_list_first(keys); l1; l1 = g_list_next(l1)) {
+			struct dfs_code *dfsc = (struct dfs_code *)l1->data;
+
+			gs->min_dfs_codes = g_list_append(gs->min_dfs_codes, 
+									dfsc);
+			if (!dfs_code_equal(
+				g_list_nth_data(gs->dfs_codes, 
+					g_list_length(gs->min_dfs_codes)-1), 
+					g_list_last(gs->min_dfs_codes)->data)) {
+
+				g_list_free(keys);
+				cleanup_map(pm_forwards);
+				g_list_free(right_most_path);
+				return 0;
+			}
+		
+			values = g_hash_table_lookup(pm_forwards, dfsc);
+			ret = projection_min(gs, values);
+
+			g_list_free(keys);
+			cleanup_map(pm_backwards);
+			cleanup_map(pm_forwards);
+			g_list_free(right_most_path);
+			return ret;
+		}
+		g_list_free(keys);
+	}
+
+	g_list_free(right_most_path);
+	cleanup_map(pm_backwards);
+	cleanup_map(pm_forwards);
+	return 1;
 }
 
 
