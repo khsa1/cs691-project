a75f813600e90fa94c6b1bfae281392e8e10ae93
John Clemens <john@deater.net>
2015-04-02 23:58:26 -0400

    Add guards and fix glib_compat.h

--- gspan-glib/glib_compat.h
+++ gspan-glib/glib_compat.h
@@ -1,16 +1,20 @@
 #include <glib.h>
 
+#ifndef __GLIB_COMPAT_H__
+#define __GLIB_COMPAT_H__
+
 #if (GLIB_MINOR_VERSION < 32)
 
 static inline gboolean g_hash_table_add(GHashTable *hash_table, gpointer key)
 {
-	return g_hash_table_replace(hash_table, key, key);
+	g_hash_table_replace(hash_table, key, key);
+	return TRUE;
 }
 
 static inline gboolean g_hash_table_contains(GHashTable *hash_table, gpointer key)
 {
-	return g_hash_table_lookup_extended(hash_table, key, key, NULL, NULL);
+	return g_hash_table_lookup_extended(hash_table, key, NULL, NULL);
 }
 
-
 #endif
+#endif /* __GLIB_COMPAT_H__ */
